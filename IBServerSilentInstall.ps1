a20b5f61fc2b087d3b4d766bcbd274c3441b5fac
pprada-sys <52825847+pprada-sys@users.noreply.github.com>
2020-02-09 17:34:34 -0500

    Commit for comments/doc and formatting

--- IBServerSilentInstall.ps1
+++ IBServerSilentInstall.ps1
@@ -1,12 +1,17 @@
-﻿# Simple Powershell Script specific for automatic, unattended, scheduled binary refresh/upgrade of
+﻿#-------------------------------------------------------------------------------------------------
+# Simple Powershell Script specific for automatic, unattended, scheduled binary installation of
 # WebFOCUS Reporting Server and iWay Data Migrator Servers
+# Creation Date: 02/09/2020
+# Developed by: Paul Prada
+#-------------------------------------------------------------------------------------------------
 
+#Setting Variabls 
 #Setting Server Name Testing with i8206_742.zip
 $BaseDir = "E:\Software\IBI\"
 $NewSrvRel = "8206_742"
 $ZipFile = "i8206_742.zip"
 
-#TODO: Getting Server Parm from input for upgrade. For now creating statically with 8206_742
+#TODO: Set Parm argument for Server zip file to install. For now creating statically with 8206_742.zip
 
 #Creating Destination Folder for server disk directory
 New-Item -ItemType Directory -Name $NewSrvRel -Path $BaseDir
